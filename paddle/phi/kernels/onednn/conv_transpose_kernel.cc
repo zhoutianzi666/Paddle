5b0d1ec539c072eeeff3886822fede56d7f4b956
K11OntheBoat <ruianmaidanglao@163.com>
2025-08-12 12:30:21 +0800

    fix onednn cpu bug

--- paddle/phi/kernels/onednn/conv_transpose_kernel.cc
+++ paddle/phi/kernels/onednn/conv_transpose_kernel.cc
@@ -375,17 +375,18 @@ void Execute(const OneDNNContext& dev_ctx,
   std::shared_ptr<dnnl::memory> bias_memory_p;
   std::shared_ptr<dnnl::memory> dst_memory_p;
   std::unordered_map<int, dnnl::memory> args;
-  
+
   // Note(ZKK):
   // Add thread_id to cache_key
   // fix issue https://github.com/PaddlePaddle/PaddleOCR/issues/15621
-  // https://github.com/PaddlePaddle/PaddleOCR/issues/15393 
-  std::string cache_key = funcs::CreateKey(dev_ctx,dev_ctx.GetInputs
-                                           phi::funcs::ThreadIDasStr(),
-                                           dev_ctx.GetInputsName("Input")[0],
-                                           dev_ctx.GetInputsName("Filter")[0],
-                                           common::vectorize(x->dims()),
-                                           common::vectorize(filter->dims()));
+  // https://github.com/PaddlePaddle/PaddleOCR/issues/15393
+  std::string cache_key =
+      funcs::CreateKey(dev_ctx,
+                       dev_ctx.GetInputs phi::funcs::ThreadIDasStr(),
+                       dev_ctx.GetInputsName("Input")[0],
+                       dev_ctx.GetInputsName("Filter")[0],
+                       common::vectorize(x->dims()),
+                       common::vectorize(filter->dims()));
   const auto& onednn_engine = dev_ctx.GetEngine();
 
   auto deconvolution_cache =
